f8706bf1421cbca017b511fee55a9cddfad3b973
tongxiaojun <tongxiaojun@redoop.com>
2024-03-09 13:01:54 +0000

    Revert "Update docusaurus.config.js"
    This reverts commit f528020f5e677cc3fdfbb99bd415235187e62c2f.

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -38,6 +38,12 @@ const config = {
           // Remove this to remove the "edit this page" links.
           editUrl: 'https://github.com/redoop/community.redoop.com/tree/main/',
         },
+        download: {
+          showReadingTime: true,
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+          editUrl: 'https://www.redoop.org/docs/install-guide/download',
+        },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
@@ -68,7 +74,7 @@ const config = {
             label: 'Documentation',
           },
           {
-            href: 'https://www.redoop.org/docs/install-guide/download',
+            href: 'https://www.redoop.org/docs/install-guide/download/',
             label: 'download',
             position: 'right',
           },
